36e403f58df1088519f755670b9dd6bc50140609
Student User <student@workstation.lab.example.com>
2020-05-21 01:26:11 +0800

    php-scale four

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,3 +1,3 @@
 <?php
-  print "This is version THREE of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+  print "This is version FOUR of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
 ?>
