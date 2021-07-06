4b0b8a14580147c899a5d4d79f19e82ab75148db
Student User <student@workstation.lab.example.com>
2021-07-06 04:09:09 -0400

    Made changes

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,3 +1,3 @@
 <?php
-  print "This is version SEVEN of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+  print "This is version EIGHT of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
 ?>
